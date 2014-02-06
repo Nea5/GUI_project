6c8f60c47dd12c91b49a666008d47ad9deef14f6
Johan Dahlkar <johan.dahlkar@gmail.com>
2014-02-06 18:38:57 +0100

    Added new task popup

--- TimeManager/src/gui/ListsPanel.java
+++ TimeManager/src/gui/ListsPanel.java
@@ -4,23 +4,40 @@ import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.LayoutManager;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.GregorianCalendar;
 
 import javax.swing.JButton;
 import javax.swing.JComboBox;
 import javax.swing.JFrame;
 import javax.swing.JList;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JTable;
 import javax.swing.table.AbstractTableModel;
+import javax.swing.table.DefaultTableModel;
 
 public class ListsPanel extends JPanel {
 	private JPanel p_north, p_west;
 	private JComboBox sort_by, filter;
 	private JButton b_new;
 	private JTable t_tasks;
-	private AbstractTableModel t_tasks_model;
+	private DefaultTableModel t_tasks_model;
 	private JScrollPane tableScroll;
+	
+	private String[] columnNames = {" ", "Task", "Date", "Category", "Priority"};
+	private Object[][] data = {
+			{
+				Boolean.FALSE, "Buy milk", new GregorianCalendar().getTime(),
+				"Home", 5
+			},
+			{
+				Boolean.FALSE, "Eat", new GregorianCalendar().getTime(),
+				"Personal", 6
+			}	
+	};
 	
 	public ListsPanel() {
 		this.setLayout(new BorderLayout());
@@ -37,33 +54,68 @@ public class ListsPanel extends JPanel {
 		p_north.add(filter);
 		p_north.add(b_new);
 		
+		b_new.addActionListener(new NewTaskListener());
+		
 		p_west.add(tableScroll);
 	}
-	
+	/*
+	 * Creates a JTable with a modified DefaultTableModel
+	 */
 	private void createTable(){
-		t_tasks_model = new TaskTableModel();
+		t_tasks_model = new DefaultTableModel(data, columnNames){
+			@Override
+		    public Class<?> getColumnClass(int col) {
+				if (col == 2){
+					return java.util.GregorianCalendar.class;
+				}
+				return (getValueAt(0, col).getClass());
+		    }
+			@Override
+			public boolean isCellEditable(int row, int col){
+				return (col == 0);
+			}
+		};
 		t_tasks = new JTable(t_tasks_model);
 		tableScroll = new JScrollPane(t_tasks);
 	}
-	
+	/*
+	 * Creates all the JComboBoxes
+	 */
 	private void createComboBox(){
 		String [] filters = { "Filter","School", "Personal", "Work"};
 		String [] sorts = { "Sort by","Date", "Priority", "Category"};
 		sort_by = new JComboBox(sorts);
 		filter = new JComboBox(filters);
 	}
-	
+	/*
+	 * Creates all the JPanels
+	 */
 	private void createPanels(){
 		p_north = new JPanel();
 		p_west = new JPanel();
 		p_north.setBackground(Color.WHITE);
 		p_west.setBackground(Color.WHITE);
 	}
-	
+	/*
+	 * Creates all the JButtons
+	 */
 	private void createButtons(){
 		b_new = new JButton("+ New Task");
 		
 	}
+	
+	public class NewTaskListener implements ActionListener{
+		public void actionPerformed(ActionEvent e){
+			NewTaskPanel p_newTask = new NewTaskPanel();
+			int result = JOptionPane.showConfirmDialog(null, p_newTask, 
+		               "Please Enter a new task", JOptionPane.OK_CANCEL_OPTION, JOptionPane.PLAIN_MESSAGE);
+			if(result == JOptionPane.OK_OPTION){
+				Object[] newTask = p_newTask.getData();
+				t_tasks_model.addRow(newTask);
+			}
+		}
+	}
+	
 	public static void main(String [] args){
 		JFrame frame = new JFrame();
 		JPanel lists = new ListsPanel();
