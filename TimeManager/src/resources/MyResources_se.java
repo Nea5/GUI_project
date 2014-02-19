ca074253d6ffd7b2f97764720d93e866a4a6beba
linneasandelin <linneasandelin@student95-5.eduroam.uu.se>
2014-02-19 10:18:38 +0100

    Språkgrejer

---
+++ TimeManager/src/resources/MyResources_se.java
@@ -0,0 +1,20 @@
+package resources;
+
+import java.util.Enumeration;
+import java.util.ResourceBundle;
+
+public class MyResources_se extends ResourceBundle {
+
+	@Override
+	public Enumeration<String> getKeys() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	protected Object handleGetObject(String key) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
