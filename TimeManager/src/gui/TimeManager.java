6d4c1b9fb9774464f2169ab6157e8e181bbba9c7
Johan Dahlkar <johan.dahlkar@gmail.com>
2014-02-28 10:38:10 +0100

    senaste

--- TimeManager/src/gui/TimeManager.java
+++ TimeManager/src/gui/TimeManager.java
@@ -1,6 +1,8 @@
 package gui;
+import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.util.Properties;
 import java.util.ResourceBundle;
@@ -33,21 +35,54 @@ public class TimeManager {
 	 * @throws IOException 
 	 */
 	
-	public static void main(String[] args) throws IOException {
-		FileInputStream in = new FileInputStream("settings.properties");
-		Properties prop = new Properties();
-		prop.load(in);
-		language = prop.getProperty("language");
+	public static void main(String[] args){
+		FileInputStream in;
+		File settings = new File("settings.properties");
+		if(!settings.exists()){
+			try {
+				settings.createNewFile();
+				FileOutputStream out = new FileOutputStream("settings.properties");
+                Properties props = new Properties();
+             	props.setProperty("language", "English");
+                props.store(out, null);
+                out.close();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+		try {
+			in = new FileInputStream("settings.properties");
+			Properties prop = new Properties();	
+			try {
+				prop.load(in);
+				language = prop.getProperty("language");
+				if(language.equals("Svenska")){
+					System.out.println("Svenskt sprak valt");
+					rb = ResourceBundle.getBundle("gui.resources.language_sv_SE");
+				} else {
+					System.out.println("English sprak valt");
+					rb = ResourceBundle.getBundle("gui.resources.language_en_GB");
+				}
+				new Controller(new MainFrame());
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				//rb = ResourceBundle.getBundle("gui.resources.language_en_GB");
+				//language = "English";
+				//new Controller(new MainFrame());
+				e.printStackTrace();
+			}	
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			//rb = ResourceBundle.getBundle("gui.resources.language_en_GB");
+			//language = "English";
+			//new Controller(new MainFrame());
+			e.printStackTrace();
+		}
+		
 		//ResourceBundle settings = ResourceBundle.getBundle("settings");
 		//language = settings.getString("language");
-		if(language.equals("Svenska")){
-			System.out.println("Svenskt sprak valt");
-			rb = ResourceBundle.getBundle("gui.resources.language_sv_SE");
-		} else {
-			System.out.println("English sprak valt");
-			rb = ResourceBundle.getBundle("gui.resources.language_en_GB");
-		}
-		new Controller(new MainFrame());
+		
 	}
 
 }
