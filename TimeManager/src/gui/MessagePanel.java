d7b0e93da322ee2a5234ead6eba1eb9573ddefa7
linneasandelin <linneasandelin@student245-244.eduroam.uu.se>
2014-03-10 11:44:54 +0100

    Fuuuuuuula hjälptexter

--- TimeManager/src/gui/MessagePanel.java
+++ TimeManager/src/gui/MessagePanel.java
@@ -1,6 +1,7 @@
 package gui;
 
 import java.awt.BorderLayout;
+
 import javax.swing.*;
 
 public class MessagePanel extends JPanel {
@@ -8,10 +9,21 @@ public class MessagePanel extends JPanel {
 	 * 
 	 */
 	private static final long serialVersionUID = 1L;
-	private JLabel textArea;
+	private JTextArea textArea;
 	
 	public MessagePanel(String msg) {
-		textArea = new JLabel(msg);
+		textArea = new JTextArea(msg);
+		
+		int columnWidth = 50;
+		textArea.setColumns(columnWidth);
+		
+		textArea.setWrapStyleWord(true);
+		textArea.setLineWrap(true);
+		textArea.setEditable(false);
+		textArea.setBackground(null);
+		
+		textArea.setSize(textArea.getPreferredSize().width, 1);
+		
 		this.setLayout(new BorderLayout());
 		this.add(textArea, BorderLayout.CENTER);
 	}
